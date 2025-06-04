1176aa7237239cbdf6185aa1e1faae71c2489581
Mothi <glendalegold6@gmail.com>
2025-06-04 11:59:46 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 
-const socket = io('https://smiling-gravel-knee.glitch.me');
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
